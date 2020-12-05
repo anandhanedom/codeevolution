e98163f0770a6f3e00134a9fc9617c534f3d1e42
Anandhan <anandhanedom@gmail.com>
2020-12-05 16:10:23 +0530

    hooks

---
+++ 001_hooks/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+//Components
+// import ClassCounter from './components/ClassCounter';
+// import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      {/* <HookCounter /> */}
+      <HookCounterTwo />
+    </div>
+  );
+}
+
+export default App;
