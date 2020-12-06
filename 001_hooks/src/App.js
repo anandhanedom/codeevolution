a3600b923bb43b9243464b6dc92ea2798ea53e05
Anandhan <anandhanedom@gmail.com>
2020-12-06 21:31:43 +0530

    s

--- 001_hooks/src/App.js
+++ 001_hooks/src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 //Components
 // import ClassCounter from './components/ClassCounter';
 // import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
+// import HookCounterTwo from './components/HookCounterTwo';
+// import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounterThree /> */}
+      <HookCounterFour />
     </div>
   );
 }
