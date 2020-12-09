e11a4508ca947b8901be1802e4c201b513bced75
Anandhan <anandhanedom@gmail.com>
2020-12-09 10:44:29 +0530

    final

--- 006_useref/src/App.js
+++ 006_useref/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
 
+//Components
+// import FocusInput from './components/FocusInput';
+// import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <FocusInput /> */}
+      {/* <ClassTimer /> */}
+      <HookTimer />
+    </div>
+  );
 }
 
 export default App;
